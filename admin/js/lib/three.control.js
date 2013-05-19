041cf801bca945f2aff9ffae2f6c810f02247fa0
Alban pasquelin <apasquelin@businesslab.com>
2013-05-19 04:06:48 +0200

    Correction admin mise a jour data base

--- admin/js/lib/three.control.js
+++ admin/js/lib/three.control.js
@@ -5,7 +5,7 @@ THREE.FirstPersonControls = function (object, domElement) {
 
 	this.domElement = ( domElement !== undefined ) ? domElement : document;
 
-	this.movementSpeed = 1.0;
+	this.movementSpeed = 0.05;
 	this.lookSpeed = 0.005;
 
 	this.lookVertical = true;
